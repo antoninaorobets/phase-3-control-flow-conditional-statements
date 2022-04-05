d6fea45c20f0864b4529ccb5c68a43c391becab7
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-04 20:01:52 -0400

    commit tree 5fdcc4cb8b92064f15f191b235b24f5301ba19c9

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,48 @@
 def admin_login(username, password)
-  # your code here
+  access = if  (password =="12345" && username=="admin")
+           "Access granted"
+         elsif (password =="12345" && username=="ADMIN")
+            "Access granted"
+          else "Access denied"
+          end
+  access
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  message =  if  temperature <40 
+               "It's brisk out there!" 
+             elsif  40 < temperature && temperature < 64
+                "It's a little chilly out there!"
+             elsif 85 < temperature 
+             "It's too dang hot out there!"
+             else "It's perfect out there!"
+              end
+    message 
 end
 
 def fizzbuzz(num)
-  # your code here
+
+  res =  if ((num % 3) == 0 && (num % 5)== 0)
+     "FizzBuzz" 
+  elsif ((num % 3) == 0) 
+    "Fizz"
+  elsif ((num % 5) == 0 )
+    "Buzz"
+  else
+    num
+  end
+  res
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  res= case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  when "nope" then puts  "Invalid operation!" 
+    return nil
+  end
+  res
 end
 
